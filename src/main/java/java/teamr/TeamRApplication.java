6e30018dee7ff0ea927d26ffe9296c609d23d0f0
yuripbong <kyri200350@gmail.com>
2025-11-22 16:31:01 +0900

    Initial Setting

---
+++ src/main/java/java/teamr/TeamRApplication.java
@@ -0,0 +1,13 @@
+package java.teamr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamRApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamRApplication.class, args);
+    }
+
+}
---
+++ src/test/java/java/teamr/TeamRApplicationTests.java
@@ -0,0 +1,13 @@
+package java.teamr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamRApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
