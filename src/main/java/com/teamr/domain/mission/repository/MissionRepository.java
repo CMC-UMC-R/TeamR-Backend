55d511685c2429ebed74ce4579d8d2d9f44e474b
dongwoo <woomacho@gmail.com>
2025-11-23 05:23:49 +0900

    dto 리스폰스 값 수정

--- src/main/java/com/teamr/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/teamr/domain/mission/repository/MissionRepository.java
@@ -5,6 +5,8 @@ import com.teamr.domain.mission.enums.DayOfWeekType;
 import com.teamr.domain.user.entity.User;
 import com.teamr.domain.mission.enums.MissionCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,7 +15,11 @@ public interface MissionRepository extends JpaRepository<Mission, Long> {
 
     Optional<Mission> findByUserIdAndDayOfWeek(Long userId, DayOfWeekType dayOfWeek);
 
-    List<Mission> findAllByUserAndDayOfWeek(User user, DayOfWeekType dayOfWeek);
+    @Query("SELECT m FROM Mission m " +
+            "LEFT JOIN FETCH m.pictureMission " +
+            "LEFT JOIN FETCH m.movementMission " +
+            "WHERE m.user = :user AND m.dayOfWeek = :dayOfWeek")
+    List<Mission> findAllByUserAndDayOfWeek(@Param("user") User user, @Param("dayOfWeek") DayOfWeekType dayOfWeek);
 
     Optional<Mission> findByUser_DeviceIdAndMissionCategory(String deviceId, MissionCategory missionCategory);
 }
