f7270e169f2ec297e528f21bf95a59a760822d2d
yuripbong <kyri200350@gmail.com>
2025-11-23 01:37:10 +0900

    docs: 캐릭터 조회 스웨거

--- src/main/java/com/teamr/domain/character/controller/CharacterController.java
+++ src/main/java/com/teamr/domain/character/controller/CharacterController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/characters")
 @RequiredArgsConstructor
-public class CharacterController {
+public class CharacterController implements CharacterSwagger {
 
     private final CharacterService characterService;
 
